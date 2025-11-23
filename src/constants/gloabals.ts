3934ee3d0fee474f28cc286403fdc947b98283e0
VictorH <pandvictor@gmail.com>
2025-11-22 20:42:36 -0600

    fix relative path images

--- src/constants/gloabals.ts
+++ src/constants/gloabals.ts
@@ -1,6 +1,7 @@
-import enData from './../../public//assets/translations/en.json';
-import esData from './../../public//assets/translations/es.json';
+import enData from '../../public/assets/translations/en.json';
+import esData from '../../public/assets/translations/es.json';
 
-export const translationsData = { 'es': esData, 'en': enData };
-export const publicPath = '/myWebsite/assets';
-export const version= "1.0.0-react-vite";
+export const translationsData = { es: esData, en: enData };
+const basePath = (import.meta.env.BASE_URL || '/').replace(/\/$/, '');
+export const publicPath = `${basePath}/assets`;
+export const version = '1.0.0-react-vite';
