8274929b6ff5be28098fa7b62149a756193be0cf
VictorH <pandvictor@gmail.com>
2025-11-22 21:32:32 -0600

    fix relative path

--- src/components/molecules/UseAvatar.tsx
+++ src/components/molecules/UseAvatar.tsx
@@ -2,11 +2,13 @@ import { Avatar, Button, Typography } from '@mui/material';
 import { publicPath } from '../../constants/gloabals';
 import { LinkItem } from '../atoms';
 import i18n from '../../utils/i18n';
+
+const basePath = (import.meta.env.BASE_URL || '/');
 export const UserAvatar = () => {
   return (
     <Button sx={{ p: 0, textTransform: 'none' }} color='inherit'>
       <Avatar sx={{ display: 'flex', mr: 1, flexGrow: 1 }} alt="A" src={`${publicPath}/images/norellanac.jpg`} />
-      <LinkItem to={`/myWebsite/`} color="inherit" relative="path" >
+      <LinkItem to={`${basePath}`} color="inherit" relative="path" >
         <Typography
           noWrap
           component="a"
